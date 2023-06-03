d9cf18b9257ebd881035b38fc840716816f99e1b
Josakko <112590914+Josakko@users.noreply.github.com>
2023-06-03 15:52:28 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+VERSION = "1.0.0"
+
+setup(
+    name="browser_stealer",
+    version=VERSION,
+    author="Josakko",
+    author_email="josakko@protonmail.com",
+    description="Python library for stealing different info from chromium based browsers",
+    packages=find_packages(),
+)
