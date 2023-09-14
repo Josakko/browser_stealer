cddfa2bfb8245549398875a3cd9e499448b3504e
Josakko <112590914+Josakko@users.noreply.github.com>
2023-09-14 17:52:21 +0200

    Add files via upload

--- build.bat
+++ build.bat
@@ -1,7 +1,17 @@
 @echo off
+setlocal enabledelayedexpansion
 
 
 echo Packaging...
-python src\setup.py bdist_wheel
+python setup.py bdist_wheel
+
+for /D %%G in ("%~dp0\*") do (
+    set "folderName=%%~nxG"
+    if "!folderName:~-9!"==".egg-info" (
+        rmdir /s /q "%%G"
+    )
+)
+rmdir /s /q "build"
+
 echo Packaging finished!
 pause
